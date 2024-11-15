d251b55863e7c485f92b1067adb109b523af1b0b
Metalax <sampannapokharel@gmail.com>
2024-11-15 12:53:08 +0545

    Refactor/ updated list

--- src/components/draggable/list/index.tsx
+++ src/components/draggable/list/index.tsx
@@ -5,30 +5,35 @@ interface ListPropsType {
 }
 
 export const List = ({ drag }: ListPropsType) => {
-    // const { propertiesState } = usePropertiesContext();
-    // const { listBulletVariation, listCount } = propertiesState.element;
-
-    // const renderLists = () => {
-    //     const elemArr = Array.from({ length: listCount }, (_, index) => (
-    //         <ListItem key={index} symbol={activeSymbol} />
-    //     ));
-    //     return elemArr;
-    // };
-
-    // style="list-style-type:disc;"
-
     return (
         <ul
             draggable
             data-category="list"
             onDragStart={(e) => drag(e)}
-            className="draggable-element w-full max-w-md space-y-1 text-gray-500 bg-white list-inside list-disc dark:text-gray-400"
+            className="draggable-element w-full px-3 space-y-1 bg-white list-inside list-disc text-black"
         >
-            <li className="flex w-full items-center">
+            <li className="flex w-full items-center list-disc">
+                <span className="mr-2">•</span>
                 <textarea
                     rows={1}
                     className="w-full max-w-full max-h-full p-2.5 text-inherit bg-gray-50 focus:outline-0"
                     placeholder="Item 1"
+                ></textarea>
+            </li>
+            <li className="flex w-full items-center list-disc">
+                <span className="mr-2">•</span>
+                <textarea
+                    rows={1}
+                    className="w-full max-w-full max-h-full p-2.5 text-inherit bg-gray-50 focus:outline-0"
+                    placeholder="Item 2"
+                ></textarea>
+            </li>
+            <li className="flex w-full items-center list-disc">
+                <span className="mr-2">•</span>
+                <textarea
+                    rows={1}
+                    className="w-full max-w-full max-h-full p-2.5 text-inherit bg-gray-50 focus:outline-0"
+                    placeholder="Item 3"
                 ></textarea>
             </li>
         </ul>
